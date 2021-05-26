fe71aaac083250118ea51a7a72ce2410cce5368b
Maverick@YesFlow <45048079+YesFlowMaverick@users.noreply.github.com>
2021-05-25 22:37:29 -0400

    Update

--- speechui-test/ios/App/App/AppDelegate.swift
+++ speechui-test/ios/App/App/AppDelegate.swift
@@ -1,15 +1,37 @@
 import UIKit
 import Capacitor
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var newBluetoothAvailable: Bool?
+    var usingBluetoothInput: Bool?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(audioRouteChanged),
+                                               name: AVAudioSession.routeChangeNotification,
+                                               object: AVAudioSession.sharedInstance())
+        let session: AVAudioSession = AVAudioSession.sharedInstance()
+        let sessionCategory: AVAudioSession.Category = .playAndRecord
+        let sessionOptions: AVAudioSession.CategoryOptions = [.allowBluetoothA2DP, .allowAirPlay, .defaultToSpeaker]
+        do {
+            // try session.setPreferredIOBufferDuration(0.01)
+            if ((session.category != sessionCategory) || !(session.categoryOptions.contains(sessionOptions))) { // TODO: add (session.ioBufferDuration != self.bufferDuration) once mode-based wakeword is enabled
+                try session.setCategory(sessionCategory, mode: .default, options: sessionOptions)
+                // TODO: The below line implicitly disables HFP output. Investigate buffer duration versus bluetooth output settings.
+                // try session.setPreferredIOBufferDuration(self.bufferDuration)
+                try session.setActive(true, options: .notifyOthersOnDeactivation)
+            }
+        } catch {
+            print("AppDelegate application error when setting AudioSession category")
+        }
         return true
     }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -56,5 +78,81 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             NotificationCenter.default.post(name: .capacitorStatusBarTapped, object: nil)
         }
     }
+
+     @objc private func audioRouteChanged(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+            let reasonValue = userInfo[AVAudioSessionRouteChangeReasonKey] as? UInt,
+            let reason = AVAudioSession.RouteChangeReason(rawValue:reasonValue) else {
+                return
+        }
+        switch reason {
+        case .newDeviceAvailable:
+            self.newBluetoothAvailable = true
+            // useBluetoothHFPInput()
+            break
+        case .oldDeviceUnavailable:
+            self.newBluetoothAvailable = false
+            self.usingBluetoothInput = false
+        default:
+            break
+        }
+    }
+
+    private func useBluetoothHFPInput() {
+        print("AppDelegate useBluetoothHFPInput")
+        let session = AVAudioSession.sharedInstance()
+        guard let inputs = session.availableInputs else {
+            return
+        }
+        for input in inputs {
+            if (input.portType == AVAudioSession.Port.bluetoothHFP) {
+                do {
+                    try session.setPreferredInput(input)
+                    self.newBluetoothAvailable = false
+                } catch {
+                    print("AppDelegate useBluetoothHFPInputIfAvailable error")
+                }
+            }
+        }
+    }
+
+    @objc public func useBluetoothHFPInputIfAvailable() {
+        print("AppDelegate useBluetoothHFPInputIfAvailable")
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playAndRecord, mode: .default, options: [.allowBluetoothA2DP, .allowAirPlay, .defaultToSpeaker, .allowBluetooth])
+            try session.setActive(true, options: .notifyOthersOnDeactivation)
+            self.useBluetoothHFPInput()
+            self.usingBluetoothInput = true
+        } catch {
+            print("AppDelegate useBluetoothHFPInputIfAvailable error when setting AudioSession category")
+        }
+    }
+
+    @objc public func useA2DPOutputIfAvailable() {
+        print("AppDelegate useA2DPOutputIfAvailable")
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playAndRecord, mode: .default, options: [.allowBluetoothA2DP, .allowAirPlay, .defaultToSpeaker])
+            try session.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("AppDelegate useA2DPOutputIfAvailable error when setting AudioSession category")
+        }
+    }
+
+    @objc public func switchInputsIfAvailable() {
+        print("AppDelegate switchInputsIfAvailable")
+        if self.newBluetoothAvailable ?? false {
+            print("AppDelegate switchInputsIfAvailable newBluetoothAvailable")
+            if self.usingBluetoothInput ?? false {
+                print("AppDelegate switchInputsIfAvailable usingBluetoothInput")
+            } else {
+                self.useBluetoothHFPInputIfAvailable()
+            }
+        } else {
+            self.useA2DPOutputIfAvailable()
+        }
+    }
+
 
 }
--- speechui-test/src/app/home/home.page.ts
+++ speechui-test/src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { YesflowSpeechUI } from 'node_modules/@capacitor-yesflow/speechui';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  ngOnInit(){
+     this.pluginInit();
+
+  }
+
+  async pluginInit() {
+    const echoResult = await YesflowSpeechUI.echo({value: 'echo this'});
+    console.log('Echoresult', echoResult);
+    const speechOptions = {
+      showPartialResults: true
+    }
+    const startSpeech = await YesflowSpeechUI.startSpeech({value: speechOptions});
+    console.log('StartSpeech', startSpeech);
+    await YesflowSpeechUI.stopSpeech({value:''});
+  }
 
 }
